0501cd4081ef737d4c0aa461b51b86586cdce671
Bianca Capretta <bianca.capretta@gmail.com>
2017-10-14 11:47:26 -0400

    Took javascript out of html file and put into data.js

--- data.js
+++ data.js
@@ -1,7 +1,42 @@
-// Code that records time intervals
+// data.js
+// Purpose: to make a sound phonate after a random amount of time and to record
+//    that in the associated google spreadsheet
 
+var times = [250, 450, 650];
+var url = 'https://script.google.com/macros/s/AKfycbyNMG8aeE9v7qAHw66EwIUirikwvlZhRC_lr5htg9V-83ZzGnE/exec';
+var actual_time = 0;
+
+// Purpose: to pick a time randomly and make it sound after x milliseconds
+function phonate()
+{
+	var randomized_time = times[Math.floor(Math.random()*times.length)];
+	var sound = document.getElementById("audio");
+	actual_time = randomized_time;
+
+	// wait some random amount of time
+	setTimeout(function() {
+		sound.play();
+	}, randomized_time);
+}
+
+// Purpose: given the assigned time, save user's perceived time and send
+//     get request to save data in Google Sheets
 function record()
 {
-	url = 'https://script.google.com/macros/s/AKfycbyNMG8aeE9v7qAHw66EwIUirikwvlZhRC_lr5htg9V-83ZzGnE/exec'
+	var perceived_time = document.getElementById("perceived_time").value;
 	
+	if (isNaN(perceived_time)) {
+		alert("Must input number");
+	}
+	else {
+		console.log("Perceived time: " + perceived_time);
+		console.log("Actual time: " + actual_time)
+
+		// clear previous input for next submission
+		document.getElementById("perceived_time").value = "";
+
+		// add to google spreadsheet
+		cordovaHTTP.get(url + '?atime=' + actual_time + "&ptime=" + perceived_time,
+											{}, {}, function(response) {});
+	}
 }
