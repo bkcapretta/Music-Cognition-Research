31fe4fccc7af770963529d10f6b38a5e30938b5d
Bianca Capretta <bianca.capretta@gmail.com>
2017-10-29 20:30:43 -0400

    Added dropdown menu to choose btw self/other; updated participant instructions

--- data.js
+++ data.js
@@ -45,3 +45,25 @@ function record()
 											{}, {}, function(response) {});
 	}
 }
+
+// Purpose: When the user clicks on the button, toggle between hiding and 
+// 	   showing the dropdown content
+function dropdown()
+{
+	document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Purpose: to close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+	if (!event.target.matches('.dropbtn')) {
+		var dropdowns = document.getElementsByClassName("dropdown-content");
+		for (var i = 0; i < dropdowns.length; i++) {
+			var openDropdown = dropdowns[i];
+			if (openDropdown.classList.contains('show')) {
+				openDropdown.classList.remove('show');
+			}
+		}
+	}
+}
+
+
