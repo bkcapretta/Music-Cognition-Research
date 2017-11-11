72ff2df560435e64f3ea833fdf0b7b32382a15be
Bianca Capretta <bianca.capretta@gmail.com>
2017-11-10 20:41:27 -0500

    Split intro and experiment into two pages, changed algorithm for random time, added beginnings of practice section

--- data.js
+++ data.js
@@ -6,22 +6,15 @@
 
 var url = 'https://script.google.com/macros/s/AKfycbyNMG8aeE9v7qAHw66EwIUirikwvlZhRC_lr5htg9V-83ZzGnE/exec';
 var actual_time = 0;
-var time_list = [100, 250, 400, 550, 700]; 
-Dictionary<int, int> time_table;
-// TODO: make algorithm so that each time is evenly selected randomly
+var time_list = [100, 100, 250, 250, 400, 400, 550, 550, 700, 700]; 
 var trial_count = 1;
 var initials = "";
 var condition = "";
 var timeline = "";
 
-function initialize(time_table)
+function initialize()
 {
-	time_table[100] = 0;
-	time_table[250] = 0;
-	time_table[400] = 0;
-	time_table[550] = 0;
-	time_table[700] = 0;
-	trial_count = 1;
+	time_table = {100: 0, 250: 0, 400: 0, 550: 0, 770: 0}
 }
 
 // Purpose: to collect the user's information
@@ -30,16 +23,14 @@ function collect()
 	initials = document.getElementById("initials").value;
 	condition = document.getElementById("condition").value;
 	//timeline = document.getElementById("timeline").value;
-
-	document.getElementById("entered").innerHTML = initials + " is ready for the experiment.";
-	//window.location.href = "#experiment";
+	console.log("Initials in collect: " + initials);
+	console.log("Condition in collect: " + condition);
 }
 
 // Purpose: to pick a time randomly and make it sound after x milliseconds
 function phonate()
 {
-	initialize();
-	var randomized_time = evenly_random();
+	var randomized_time = time_list[Math.floor(Math.random()*time_list.length)];
 	var sound = new Audio("http://www.soundjay.com/button/beep-07.wav"); // tone_100ms.wav
 	actual_time = randomized_time;
 
@@ -49,13 +40,31 @@ function phonate()
 	}, randomized_time);
 }
 
+function evenly_random()
+{
+	var randomized_time = time_list[Math.floor(Math.random()*time_list.length)];
+	console.log("I'm in EVENLY_RANDOM()")
+	var num_times = time_table["randomized_time"];
+
+	while (num_times == 2) {
+		console.log("This time (" + randomized_time + ") has been seen twice" +
+			" before");
+		randomized_time = time_list[Math.floor(Math.random()*time_list.length)];
+		num_times = time_table["randomized_time"];
+	}
+
+	num_times++;
+	time_table["randomized_time"] = num_times;
+
+	return randomized_time;
+}
+
 // Purpose: given the assigned time, save user's perceived time and send
 //     get request to save data in Google Sheets
 function record()
 {
 	var perceived_time = document.getElementById("perceived_time").value;
 	var user_info = window.location.search;
-	console.log("User info: " + user_info);
 	
 	// TODO: make screen stay at the lower marker
 	if (isNaN(perceived_time)) {
@@ -65,7 +74,7 @@ function record()
 		// testing
 		console.log("Initial: " + initials);
 		console.log("Condition: " + condition);
-		console.log("Timeline: " + timeline);
+		// console.log("Timeline: " + timeline);
 		console.log("Trial count: " + trial_count);
 		console.log("Perceived time: " + perceived_time);
 		console.log("Actual time: " + actual_time)
@@ -77,33 +86,19 @@ function record()
 		cordovaHTTP.get(url + '?atime=' + actual_time + "&ptime=" + perceived_time 
 			+ '&initials=' + initials + '&condition=' + condition + 
 			'&timeline=' + timeline +'&trial=' + trial_count, {}, {}, function(response) {});
-	
-		// jump to experiment part of page
-		//window.location.href = "#experiment";
 
 		trial_count++;
-		if (trial_count == 31)
+		
+		if (trial_count == 11)
 		{
-			alert("You finished 30 trials for this block.");
+			alert("You finished 10 trials for this block.");
 			initialize();	
+			trial_count = 1;
 		}
 
 		// adds trial number
-		document.getElementById("count").innerHTML = "Trial: " + trial_count + "/30";
+		document.getElementById("count").innerHTML = "Trial: " + trial_count + "/10";
 	}
-}
-
-function evenly_random()
-{
-	var randomized_time = time_list[Math.floor(Math.random()*time_list.length)];
-	
-	while (time_table[randomized_time] == 6) {
-		randomized_time = time_list[Math.floor(Math.random()*time_list.length)];
-	}
-
-	time_table[randomized_time]++;
-	console.log("Random time:" + randomized_time);
-	return randomized_time;
 }
 
 
