87642b93393d7c4865a4e73f54a97eb1182d79a0
Bianca Capretta <bianca.capretta@gmail.com>
2017-12-12 16:29:50 -0500

    Figured out randomization for async experiment; slowed down average to 850ms

--- Asynchrony/data.js
+++ Asynchrony/data.js
@@ -6,7 +6,12 @@ var url = 'https://script.google.com/macros/s/AKfycbyNMG8aeE9v7qAHw66EwIUirikwvl
 var team_points = 0;
 //var time_list = [338,410,483,746,748,570,446,661,611,723,760,777,365,502,801,660,675,395,374,850,304,579,473,807,638,659,450,322,841,535,717,760,827,804,657,813,732,422,545,435,668,765,670,754,875,887,784,610,723,613,494,595,353,800,393,598,508,619,499,670,574,351,306,478,575,404,422]
 // average number is 120 which is ~600ms
-var time_list = [171,75,131,92,116,66,120,64,66,169,123,136,143,94,73,104,160,171,180,172,136,129,128,96,63,124,152,159,111,136,85,101,77,127,148,100,141,67,176,129,67,73,82,119,159,177,115,151,139,99,64,165,99,138,176,106,101,172,74,89,74,148,141,128,75,179,120]
+//var time_listP = [171, 75, 131, 92, 116, 66, 120, 64, 66, 169, 123, 136, 143, 94, 73, 104, 160, 171, 180, 172, 136, 129, 128, 96, 63, 124, 152, 159, 111, 136, 85, 101, 77, 127, 148, 100, 141, 67, 176, 129, 67, 73, 82, 119, 159, 177, 115, 151, 139, 99, 64, 165, 99, 138, 176, 106, 101, 172, 74, 89, 74, 148, 141, 128, 75, 179, 120];
+//var time_listE = [171, 75, 131, 92, 116, 66, 120, 64, 66, 169, 123, 136, 143, 94, 73, 104, 160, 171, 180, 172, 136, 129, 128, 96, 63, 124, 152, 159, 111, 136, 85, 101, 77, 127, 148, 100, 141, 67, 176, 129, 67, 73, 82, 119, 159, 177, 115, 151, 139, 99, 64, 165, 99, 138, 176, 106, 101, 172, 74, 89, 74, 148, 141, 128, 75, 179, 120];
+
+
+var time_listP = [213, 233, 161, 278, 234, 243, 228, 288, 192, 180, 227, 291, 151, 248, 178, 210, 171, 229, 188, 267, 155, 164, 255, 225, 248, 176, 204, 256, 235, 165, 290, 235, 169, 208, 249, 185, 266, 256, 184, 220, 201, 281, 228, 152, 265, 287, 187, 193, 165, 180, 229, 205, 286, 171, 234, 273, 294, 223, 157, 226, 247, 177,	256, 296, 266, 286,	264, 239, 289, 279, 196, 296, 286, 249,	274, 165, 225, 224,	288, 236, 218, 182,	188, 259, 183, 282,	202, 249, 223, 259, 221, 192, 155, 274,	182, 179, 154, 182,	289, 159];
+var time_listE = [213, 233, 161, 278, 234, 243, 228, 288, 192, 180, 227, 291, 151, 248, 178, 210, 171, 229, 188, 267, 155, 164, 255, 225, 248, 176, 204, 256, 235, 165, 290, 235, 169, 208, 249, 185, 266, 256, 184, 220, 201, 281, 228, 152, 265, 287, 187, 193, 165, 180, 229, 205, 286, 171, 234, 273, 294, 223, 157, 226, 247, 177,	256, 296, 266, 286,	264, 239, 289, 279, 196, 296, 286, 249,	274, 165, 225, 224,	288, 236, 218, 182,	188, 259, 183, 282,	202, 249, 223, 259, 221, 192, 155, 274,	182, 179, 154, 182,	289, 159];
 
 // Purpose: to start the timer and have the experimenter last one minute; have
 //     the buttons randomly appear on either side of the screen
@@ -16,7 +21,8 @@ function start()
 	var element = document.getElementById("start");
 	element.parentNode.removeChild(element);
 	
-	countDown();
+	countDownParticipant();
+	countDownExperimenter();
 }
 
 // Purpose: to begin the simulation of practicing the activity
@@ -43,26 +49,62 @@ function done()
 }
 
 // Purpose: to track 40 seconds of time during the activity
-function countDown()
+function countDownParticipant()
 {
 	var startTime = new Date();
 	var endingTime = new Date(startTime.getTime() + 40000);
 	var ms_count = 1;
+	var randomized_time = 200;
 	
 	var x = setInterval(function() {
 		// get current time and find distance between now and end
 		var current = new Date();
 		var distance = endingTime - current;
-		var randomNum = randomize();
-
-		// make a random user's button appear a random amount of time
-		// on average every 600ms
-		if (ms_count == 60) {
+		
+		// make participant's button appear on average every 600ms
+		if (ms_count == 100) {
 			clearButton("P");
-			clearButton("E");
+			// randomly gets a number from the time_list at the top
+			randomized_time = randomize(time_listP);
 		}
-		if (ms_count == randomNum) { // make button appear some random number
-			display("P");	      		// of seconds
+		// make button appear some random number of seconds after being cleared
+		if (ms_count == randomized_time) { 
+			display("P");
+			ms_count = 0;
+		}
+		ms_count++;
+		
+		if (distance < 0) { // if count down is finished, display to screen
+			clearInterval(x);
+			done();
+		}
+	}, 1); // update interval every millisecond
+}
+
+// Purpose: to track 40 seconds of time during the activity
+function countDownExperimenter()
+{
+	var startTime = new Date();
+	var endingTime = new Date(startTime.getTime() + 40000);
+	var ms_count = 1;
+	var randomized_time = 200;
+	
+	var x = setInterval(function() {
+		// get current time and find distance between now and end
+		var current = new Date();
+		var distance = endingTime - current;
+		//var randomNum = randomize();
+		//console.log("Randomnum: " + randomNum);
+		
+		// buttons appear on average every 850ms 
+		// (could go back to 600ms where ms_count was 60 but it was very fast)
+		if (ms_count == 100) {
+			clearButton("E");
+			// randomly gets a number from the time_list saved at the top
+			randomized_time = randomize(time_listE);
+		}
+		// make button appear some random number of seconds after being cleared
+		if (ms_count == randomized_time) { 
 			display("E");
 			ms_count = 0;
 		}
@@ -75,23 +117,27 @@ function countDown()
 	}, 1); // update interval every millisecond
 }
 
+
 // Purpose: to simulate the experiment for the participant lasting 10 seconds
 function practiceCountDown()
 {
 	var startTime = new Date();
 	var endingTime = new Date(startTime.getTime() + 10000); // 10 seconds
 	var ms_count = 1;
+	var randomized_time = 120;
 	
 	var x = setInterval(function() {
 		// get current time and find distance between now and end
 		var current = new Date();
 		var distance = endingTime - current;
 
-		// buttons appear for 500ms and then off for 500ms
-		if (ms_count == 60) {
+		// buttons appear on average every 850ms 
+		// (could go back to 600ms where ms_count was 60 but it was very fast) 
+		if (ms_count == 100) {
 			clearButton("P");
+			randomized_time = randomize(time_listE);
 		}
-		if (ms_count == 120) { 
+		if (ms_count == randomized_time) { 
 			display("P");               
 			ms_count = 0;
 		}
@@ -124,7 +170,7 @@ function clearButton(user)
 
 // Purpose: to generate a random number that determines how long the user waits
 //     to press a button (avg during the minute needs to be 600ms)
-function randomize()
+function randomize(time_list)
 {
 	// randomly gets a number from the time_list saved at the top
 	var randomized_time = time_list[Math.floor(Math.random()*time_list.length)];
